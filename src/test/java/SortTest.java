f0ece528280edaf9e711b57a30804f7a6526efad
Dmitry Malchikov <dmalch@gmail.com>
2013-01-29 14:47:01 +0400

    Written failing test

---
+++ src/test/java/SortTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class SortTest {
+
+    public static final int[] UNSORTED_ARRAY = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static final int[] SORTED_ARRAY = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    @Test
+    public void testSortIsCorrect() throws Exception {
+        final int[] unsortedArray = givenUnsortedArray();
+
+        final int[] sortedArray = whenSort(unsortedArray);
+
+        thenTheArrayIsSorted(sortedArray);
+    }
+
+    private void thenTheArrayIsSorted(final int[] sortedArray) {
+        assertThat(sortedArray, equalTo(SORTED_ARRAY));
+    }
+
+    private int[] whenSort(final int[] unsortedArray) {
+        return unsortedArray;
+    }
+
+    private int[] givenUnsortedArray() {
+        return UNSORTED_ARRAY;
+    }
+}
