adc769f5a6a6d0e5f2f642418c07ecfc7d47d6f8
Dmitry Malchikov <dmalch@gmail.com>
2013-02-11 19:35:43 +0400

    Refactor heap sort test

--- src/test/java/com/dmalch/priority/UnorderedArrayPriorityQueueTest.java
+++ src/test/java/com/dmalch/priority/UnorderedArrayPriorityQueueTest.java
@@ -1,38 +1,9 @@
 package com.dmalch.priority;
 
-import org.junit.Test;
+public class UnorderedArrayPriorityQueueTest extends AbstractPriorityQueueTest {
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-public class UnorderedArrayPriorityQueueTest {
-    public static final Integer[] UNSORTED_ARRAY = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static final Integer[] TOP_5_FROM_ARRAY = {12, 11, 10, 9, 8};
-    public static final Integer[] EMPTY_ARRAY = new Integer[]{};
-
-    @Test
-    public void testGeneralArray() throws Exception {
-        doTest(UNSORTED_ARRAY, TOP_5_FROM_ARRAY);
-    }
-
-    @Test
-    public void testEmptyArray() throws Exception {
-        doTest(EMPTY_ARRAY, EMPTY_ARRAY);
-    }
-
-    private void doTest(final Integer[] unsortedArray, final Integer[] expectedArray) {
-        final UnorderedArrayPriorityQueue priorityQueue = givenPriorityQueueWithSize(5);
-
-        final Integer[] top5 = whenAddValuesToQueue(unsortedArray, priorityQueue);
-
-        assertThat(top5, equalTo(expectedArray));
-    }
-
-    private Integer[] whenAddValuesToQueue(final Integer[] unsortedArray, final UnorderedArrayPriorityQueue priorityQueue) {
-        return priorityQueue.addAllAndDeleteMin(unsortedArray);
-    }
-
-    private UnorderedArrayPriorityQueue givenPriorityQueueWithSize(final int maxSize) {
+    @Override
+    protected UnorderedArrayPriorityQueue givenPriorityQueueWithSize(final int maxSize) {
         return new UnorderedArrayPriorityQueue(maxSize);
     }
 }
